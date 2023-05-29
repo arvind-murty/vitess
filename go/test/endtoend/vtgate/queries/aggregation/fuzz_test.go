b1dbab5d3e455502f513001d06162a7fb6b61188
Arvind Murty <10248018+arvind-murty@users.noreply.github.com>
2023-05-29 00:03:31 -0700

    draft for simplifying fuzz_test
    Signed-off-by: Arvind Murty <10248018+arvind-murty@users.noreply.github.com>

--- go/test/endtoend/vtgate/queries/aggregation/fuzz_test.go
+++ go/test/endtoend/vtgate/queries/aggregation/fuzz_test.go
@@ -18,10 +18,18 @@ package aggregation
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"math/rand"
 	"strings"
 	"testing"
 	"time"
+	"vitess.io/vitess/go/mysql/collations"
+	"vitess.io/vitess/go/slices2"
+	"vitess.io/vitess/go/vt/key"
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	"vitess.io/vitess/go/vt/sqlparser"
+	"vitess.io/vitess/go/vt/vtgate/simplifier"
+	"vitess.io/vitess/go/vt/vtgate/vindexes"
 
 	"golang.org/x/exp/maps"
 )
@@ -37,19 +45,100 @@ type (
 	}
 )
 
+type schemaInfo struct {
+	schema map[string]tableT
+}
+
+func (s schemaInfo) FindTableOrVindex(tablename sqlparser.TableName) (*vindexes.Table, vindexes.Vindex, string, topodatapb.TabletType, key.Destination, error) {
+	t := s.schema[tablename.Name.String()]
+	cols := slices2.Map(t.columns, func(from column) vindexes.Column {
+		return vindexes.Column{
+			Name:          sqlparser.NewIdentifierCI(from.name),
+			Type:          0,
+			CollationName: "",
+		}
+	})
+	tbl := &vindexes.Table{
+		Type: "base",
+		Name: sqlparser.IdentifierCS{},
+		Keyspace: &vindexes.Keyspace{
+			Name:    "ks",
+			Sharded: true,
+		},
+		Columns:                 cols,
+		ColumnListAuthoritative: true,
+	}
+
+	return tbl, nil, "ks", topodatapb.TabletType_REPLICA, nil, nil
+
+}
+
+func (s schemaInfo) ConnCollation() collations.ID {
+	return collations.Unknown
+}
+
+func TestSimplifyBuggyQuery(t *testing.T) {
+	mcmp, closer := start(t)
+	defer closer()
+
+	t1Insert := "insert into t1 (t1_id, name, value, shardKey) values (0, 'name0', 'value0', 0),(1, 'name1', 'value1', 1),(2, 'name2', 'value2', 2),(3, 'name3', 'value3', 3),(4, 'name4', 'value4', 4),(5, 'name5', 'value5', 5),(6, 'name6', 'value6', 6),(7, 'name7', 'value7', 7),(8, 'name8', 'value8', 8),(9, 'name9', 'value9', 9),(10, 'name10', 'value10', 10),(11, 'name11', 'value11', 11),(12, 'name12', 'value12', 12),(13, 'name13', 'value13', 13),(14, 'name14', 'value14', 14),(15, 'name15', 'value15', 15),(16, 'name16', 'value16', 16),(17, 'name17', 'value17', 17);"
+	t2Insert := "insert into t2 (id, shardKey) values (0, 0);"
+
+	mcmp.Exec(t1Insert)
+	mcmp.Exec(t2Insert)
+
+	query := "select /*vt+ PLANNER=Gen4 */ count(tbl0.t1_id), count(tbl0.t1_id), count(tbl0.name) from t1 as tbl0 group by tbl0.value, tbl0.value order by tbl0.value limit 14"
+
+	stmt, _, err := sqlparser.Parse2(query)
+	require.NoError(t, err)
+	rewritten, _ := sqlparser.RewriteAST(sqlparser.CloneStatement(stmt), "ks", sqlparser.SQLSelectLimitUnset, "", nil, nil)
+
+	info := schemaInfo{}
+	info.schema = map[string]tableT{
+		"t1": {name: "t1", columns: []column{
+			{name: "t1_id", typ: "bigint"},
+			{name: "name", typ: "varchar"},
+			{name: "value", typ: "varchar"},
+			{name: "shardKey", typ: "bigint"},
+		}},
+		"t2": {name: "t2", columns: []column{
+			{name: "id", typ: "bigint"},
+			{name: "shardKey", typ: "bigint"},
+		}},
+	}
+	simplified := simplifier.SimplifyStatement(
+		rewritten.AST.(sqlparser.SelectStatement),
+		"ks",
+		info,
+		func(statement sqlparser.SelectStatement) (failed bool) {
+			q := sqlparser.String(statement)
+			t.Run(q, func(t *testing.T) {
+				mcmp, closer := start(t)
+				defer closer()
+
+				_, _ = mcmp.ExecAllowAndCompareError(q)
+				failed = t.Failed()
+			})
+			return
+		},
+	)
+
+	fmt.Println(sqlparser.String(simplified))
+}
+
 func TestFuzzAggregations(t *testing.T) {
 	// This test randomizes values and queries, and checks that mysql returns the same values that Vitess does
 	mcmp, closer := start(t)
 	defer closer()
 
-	noOfRows := 3
+	noOfRows := rand.Intn(20)
 	var values []string
 	for i := 0; i < noOfRows; i++ {
 		values = append(values, fmt.Sprintf("(%d, 'name%d', 'value%d', %d)", i, i, i, i))
 	}
 	t1Insert := fmt.Sprintf("insert into t1 (t1_id, name, value, shardKey) values %s;", strings.Join(values, ","))
 	values = nil
-	noOfRows = 2
+	noOfRows = rand.Intn(20)
 	for i := 0; i < noOfRows; i++ {
 		values = append(values, fmt.Sprintf("(%d, %d)", i, i))
 	}
